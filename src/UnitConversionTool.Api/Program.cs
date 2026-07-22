b71c71216a8cfd379c15d77dd092e5f1691d7d75
DipanjaliDP <dipanjalipise2000@gmail.com>
2026-07-22 12:47:16 +0530

    feat: register conversion service and configure Swagger pipeline

--- src/UnitConversionTool.Api/Program.cs
+++ src/UnitConversionTool.Api/Program.cs
@@ -1,16 +1,29 @@
+using UnitConversionTool.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new()
+    {
+        Title = "Unit Conversion API",
+        Version = "v1",
+        Description = "Convert numerical values between supported units of measurement."
+    });
+});
+
+builder.Services.AddSingleton<IUnitConversionService, UnitConversionService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Unit Conversion API v1");
+    options.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
